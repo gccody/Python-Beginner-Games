2c10d6f161de13d5a4b5b2062c191ace02f9d457
Grant <gccody2010@live.com>
2021-09-29 12:20:40 -0400

    Update RandomPasswordGenerator.py

---
+++ RandomPasswordGenerator.py
@@ -0,0 +1,26 @@
+from random import *
+import os
+
+os.system('cls')
+
+def getPassword():
+    length = randint(20, 30)
+
+    llist = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '!', '@', '#', '$', '%', '&',]
+    shuffle(llist)
+
+    password = ""
+
+    for i in range(length):
+        symbol = llist[randint(0, len(llist) - 1)]
+        password += symbol
+    return password
+
+def main():
+    os.system('cls')
+    password = getPassword()
+    print(password)
+    input("")
+
+if __name__ == '__main__':
+    main()
