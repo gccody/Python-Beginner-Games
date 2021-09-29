eefda82dd1bf243ca5de580697950012fd31212f
Grant <gccody2010@live.com>
2021-09-29 12:21:00 -0400

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -1,3 +1,11 @@
 import os
 
-os.system('cls')
+def instructions():
+    os.system('cls')
+    print("This is a two player game. Please have one player enter the word")
+
+def main():
+    os.system('cls')
+
+if __name__ == '__main__':
+    main()
