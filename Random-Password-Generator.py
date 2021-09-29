43425e39f6ef1cd92e469ed4e30d25bafce0dc6a
Grant <gccody2010@live.com>
2021-09-29 11:54:26 -0400

    Update Random-Password-Generator.py

--- Random-Password-Generator.py
+++ Random-Password-Generator.py
@@ -1,6 +1,8 @@
 from random import *
 import os
 
+os.system('cls')
+
 length = randint(20, 30)
 
 llist = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '!', '@', '#', '$', '%', '&',]
